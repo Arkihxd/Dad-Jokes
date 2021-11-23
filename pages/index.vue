79617b8e824ec879efa0b9865dc8b03ebae69cf3
Mateus Oliveira <mateusoliveiradesouza@hotmail.com>
2021-11-23 12:34:07 -0300

    Create project and get jokes

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Default/>
+  </div>
+</template>
+
+<script lang="ts">
+import Default from '../layouts/default.vue'
+export default({
+   head(){
+    return {
+      title: 'Welcome To Dad Jokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny dad jokes'
+        }
+      ]
+    }
+  },
+  components: {
+    Default
+  }
+})
+</script>
